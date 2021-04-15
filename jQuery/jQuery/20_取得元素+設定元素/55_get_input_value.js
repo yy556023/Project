bcc036cbf51c9dd4608c4ea69c8b4aea3654a50b
Yuu <yy556023@gmail.com>
2021-04-15 19:44:58 +0800

    main

---
+++ jQuery/jQuery/20_取得元素+設定元素/55_get_input_value.js
@@ -0,0 +1,14 @@
+// 10. 如果是這樣寫的話，有錯誤訊息，為什麼？
+// btnGet.onclick = function(){
+
+// 11. 畫面準備好以後才開始執行
+$(function () {
+  btnGet.onclick = function () {
+    console.log("OK");
+    console.log($("#userName").prop("value"));
+    console.log($("#phone").prop("value"));
+    console.log($("#address").prop("value"));
+    console.log($("input:nth-child(5)").attr("value"));
+    // console.log($("input:nth-child(5)").attr("checked"));
+  };
+});
