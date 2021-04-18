991b671e7d383f5df45e082b4c079fb6fbe816d0
Yuu <yy556023@gmail.com>
2021-04-18 17:12:57 +0800

    .

--- jQuery/30_css樣式/20_addClass_removeClass.js
+++ jQuery/30_css樣式/20_addClass_removeClass.js
@@ -1,7 +1,35 @@
 // 試試看 addClass、removeClass，以及各種選擇器
 
-// 10. div,p
-// 20. div>p
-// 30. div+p
-// 40. div~p
-// 50. div p
+// 10. div,p btn1
+// 20. div>p btn2
+// 30. div+p btn3
+// 40. div~p btn4
+// 50. div p btn5
+$().ready(function () {
+  var p1 = $("div,p");
+  var p2 = $("div>p");
+  var p3 = $("div+p");
+  var p4 = $("div~p");
+  var p5 = $("div p");
+
+  $("#btn1").on("click", function () {
+    p1.removeClass("bgColor");
+    p1.addClass("bgColor");
+  });
+  $("#btn2").on("click", function () {
+    p1.removeClass("bgColor");
+    p2.addClass("bgColor");
+  });
+  $("#btn3").on("click", function () {
+    p1.removeClass("bgColor");
+    p3.addClass("bgColor");
+  });
+  $("#btn4").on("click", function () {
+    p1.removeClass("bgColor");
+    p4.addClass("bgColor");
+  });
+  $("#btn5").on("click", function () {
+    p1.removeClass("bgColor");
+    p5.addClass("bgColor");
+  });
+});
