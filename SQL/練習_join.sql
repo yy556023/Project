985d0bcd03ee5f975143b005e7942366b316b858
Yuu <yy556023@gmail.com>
2021-05-05 23:27:06 +0800

    2021/05/05SQL練習完成

--- SQL/練習_join.sql
+++ SQL/練習_join.sql
@@ -14,7 +14,7 @@ FROM
 	FROM [Order Details]
 	GROUP BY OrderID
 )AS A,
-	(
+(
 	SELECT MAX(Total) Total
 	FROM
 	(
@@ -22,19 +22,66 @@ FROM
 		FROM [Order Details]
 		GROUP BY OrderID
 	)AS C
-	)AS B
+)AS B
 WHERE A.Total = B.Total
 
 
 -- 350. VINET的訂單中，有幾個業務經手 (顯示總數) (COUNT + 子查詢)
+SELECT COUNT(EmployeeID) 業務數量
+FROM
+(
+	SELECT DISTINCT EmployeeID
+	FROM Orders
+	WHERE CustomerID LIKE 'VINET'
+)AS A
+
 
 -- 360. VINET的訂單中，所有經手業務的名字 (JOIN)
-
+SELECT DISTINCT LastName,FirstName
+FROM Orders AS O INNER JOIN Employees AS E
+	ON O.EmployeeID = E.EmployeeID
+WHERE CustomerID LIKE 'VINET'
 
 
 -- 370. VINET的訂單中，找出訂單編號為10274所購買的產品清單，請找出產品名稱、產品價格
-SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = 'Production'
+SELECT O.OrderID,P.ProductID,P.ProductName,OD.UnitPrice
+FROM Orders AS O INNER JOIN [Order Details] AS OD
+ON O.OrderID = OD.OrderID INNER JOIN Products AS P
+ON OD.ProductID = P.ProductID
+WHERE CustomerID LIKE 'VINET' AND O.OrderID = 10274
 
 
 -- 380. 依照貨運公司，哪一家貨運公司承接最多訂單金額 (加總訂單價格)
+SELECT TOP 1 ShipVia,SUM(Total) Total
+FROM
+(
+	SELECT O.OrderID,SUM(UnitPrice*Quantity*(1-Discount)) Total,ShipVia
+	FROM Orders AS O INNER JOIN [Order Details] AS OD
+	ON O.OrderID = OD.OrderID
+	GROUP BY O.OrderID,ShipVia
+)AS A
+GROUP BY ShipVia
+ORDER BY Total DESC
 
+-- SELECT ShipVia,A.Total
+-- FROM
+-- (
+-- 	SELECT *
+-- 	FROM SHIP
+-- )AS A,
+-- (
+-- 	SELECT MAX(Total) Total
+-- 	FROM SHIP
+-- )AS B
+-- WHERE A.Total = B.Total
+
+--CREATE VIEW SHIP AS
+--	SELECT ShipVia,SUM(Total) Total
+--	FROM
+--	(
+--		SELECT O.OrderID,SUM(UnitPrice*Quantity*(1-Discount)) Total,ShipVia
+--		FROM Orders AS O INNER JOIN [Order Details] AS OD
+--		ON O.OrderID = OD.OrderID
+--		GROUP BY O.OrderID,ShipVia
+--	)AS A
+--	GROUP BY ShipVia
--- SQL/練習_subquery.sql
+++ SQL/練習_subquery.sql
@@ -1,18 +1,48 @@
- 
+
 
 -- 410 請列出各項產品的類別、編號、品名、單價、
 --     該類產品平均單價、單價與產品平均單價的「價差」。
-
+SELECT P.CategoryID,ProductID,ProductName,UnitPrice,[AVG] 平均單價,(UnitPrice-[AVG]) 價差
+FROM Products AS P,
+(
+    SELECT AVG(UnitPrice) AVG,CategoryID
+    FROM Products
+    GROUP BY CategoryID
+)AS A
+WHERE P.CategoryID = A.CategoryID
+ORDER BY CategoryID
 
 
 -- 420 請列出有訂第三類產品的訂單
-
-
+SELECT  OrderID,CategoryID --OrderID
+FROM [Order Details] AS A,
+(
+   SELECT *
+   FROM Products
+   WHERE CategoryID = 3
+)AS B
+WHERE A.ProductID = B.ProductID
+ORDER BY OrderID
 
 
 -- 430 請列出 銷售數量最多 的前三項產品
-
-
+SELECT TOP 3 A.ProductID,A.ProductName,B.銷售數量
+FROM Products AS A,
+(
+    SELECT ProductID,SUM(Quantity) 銷售數量
+    FROM [Order Details]
+    GROUP BY ProductID
+)AS B
+WHERE A.ProductID = B.ProductID
+ORDER BY B.銷售數量 DESC
 
 
 -- 440 有哪些產品是日商提供的?
+SELECT A.*
+FROM Products AS A,
+(
+    SELECT *
+    FROM Suppliers
+    WHERE Country LIKE 'Japan'
+)AS B
+WHERE A.SupplierID = B.SupplierID
